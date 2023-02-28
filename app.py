ced5e07b1c7f55a2d405efb9dec5eae42cf58201
장두혁 <jjd0324@gmail.com>
2023-02-28 21:06:40 +0900

    feat: post기능

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://sparta:test@cluster0.gm0wapr.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/mars", methods=["POST"])
+def mars_post():
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+
+    doc = {
+        'name': name_receive,
+        'address': address_receive,
+        'size': size_receive
+    }
+
+    db.mars.insert_one(doc)
+
+    return jsonify({'msg': '주문 완료!'})
+
+@app.route("/mars", methods=["GET"])
+def mars_get():
+    return jsonify({'msg':'GET 연결 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5010, debug=True)
